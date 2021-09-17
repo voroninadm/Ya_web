a292053b580267a4f37eeeb6ca34c800f58f34c7
Ars <68727643+voroninadm@users.noreply.github.com>
2021-09-17 14:52:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let phrases = [
+    { text: 'отправить Роме и Уле смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
+    { text: 'посмотреть скидки на авиабилеты в Египет', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
+    { text: 'послушать новую говномузыку', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
+    { text: 'посмотреть Дудя на ютубе', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
+    { text: 'полить цветы еще раз', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
+    { text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
+    { text: 'почитать новости и испортить себе настроение', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
+    { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
+    { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
+    { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
+];
+
+function getRandomElement(arr) {
+    let randIndex = Math.floor(Math.random() * arr.length);
+    return arr[randIndex];
+}
+
+let button = document.querySelector('.button');
+let phrase = document.querySelector('.phrase');
+let advice = document.querySelector('.advice');
+let image = document.querySelector('.image');
+
+button.addEventListener('click', function () {
+    let randomElement = getRandomElement(phrases);
+    smoothly(phrase, 'textContent', randomElement.text)
+    smoothly(image, 'src', randomElement.image)
+
+    if (randomElement.text.length > 40) {
+        advice.style.fontSize = '33px';
+    } else {
+        advice.style.fontSize = '42px';
+    }
+});
+
+for (let i = 0; i <= 2; i = i + 1){
+    smoothly(phrase,'textContent',phrases[i].text );
+    smoothly(image, 'src', phrases[i].image)
+}
